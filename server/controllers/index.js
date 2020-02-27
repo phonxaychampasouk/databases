02dbd42d2561615571529af1968b119e3e8919d7
phonxaychampasouk <phonxay.champasouk@outlook.com>
2020-02-26 21:03:16 -0800

    Write client-server-database communication functions

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -5,9 +5,9 @@ var models = require('../models');
 module.exports = {
   messages: {
     get: function (req, res) { // a function which handles a get request for all messages
-    // 1) ask models to make the SQL request
-    // 2) send back the response
-      models.messages.get( (err, results) => {
+      // 1) ask models to make the SQL request
+      // 2) send back the response
+      models.messages.get((err, results) => {
         if (err) {
           res.statusCode = 404;
           res.end();
@@ -19,13 +19,47 @@ module.exports = {
       });
     },
 
-    post: function (req, res) {} // a function which handles posting a message to the database
-  },
+    post: function (req, res) { // a function which handles posting a message to the database
+      models.messages.post((err) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end();
+        } else {
+          res.statusCode = 200;
+          res.end();
+        }
+      });
+    },
 
-  users: {
-    // Ditto as above
-    get: function (req, res) {},
-    post: function (req, res) {}
+    users: {
+      // Ditto as above
+      get: function (req, res) { // a function which handles a get request for all users
+        // 1) ask models to make the SQL request
+        // 2) send back the response
+        models.users.get((err, results) => {
+          if (err) {
+            res.statusCode = 404;
+            res.end();
+          } else {
+            res.statusCode = 200;
+            res.write(JSON.stringify(results));
+            res.end();
+          }
+        });
+      },
+
+      post: function (req, res) { // a function which handles posting a message to the database
+        models.users.post((err) => {
+          if (err) {
+            res.statusCode = 404;
+            res.end();
+          } else {
+            res.statusCode = 200;
+            res.end();
+          }
+        });
+      }
+    }
   }
 };
 
--- server/models/index.js
+++ server/models/index.js
@@ -12,17 +12,45 @@ module.exports = {
         } else {
           callback(null, results);
         }
-      // Send it back to the client
+        // Send it back to the client
       });
     }, // a function which produces all the messages
-    post: function () {} // a function which can be used to insert a message into the database
+    post: function (message, callback) {
+      var sql = `INSERT INTO messages (messageText, roomname, userID) VALUES (${message.text}, ${message.roomname}, ${message.username})`;
+      db.connection.query(sql, function (error, results, fields) {
+        if (error) {
+          callback(error, null);
+        } else {
+          callback(null, null);
+        }
+        // Send it back to the client
+      });
+    } // a function which can be used to insert a message into the database
   },
 
   users: {
     // Ditto as above.
-    get: function () {},
-    post: function () {}
+    get: function (callback) {
+      // Ask the SQL database for a list of all the messages, including username and roomname
+      db.connection.query('SELECT * FROM users', function (error, results, fields) {
+        if (error) {
+          callback(error, null);
+        } else {
+          callback(null, results);
+        }
+        // Send it back to the client
+      });
+    }, // a function which produces all the messages
+    post: function (user, callback) {
+      var sql = `INSERT INTO users (username) VALUES (${user.username})`;
+      db.connection.query(sql, function (error, results, fields) {
+        if (error) {
+          callback(error, null);
+        } else {
+          callback(null, null);
+        }
+        // Send it back to the client
+      });
+    }
   }
 };
-
-module.exports.messages.get();
