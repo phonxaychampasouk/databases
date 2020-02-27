1a6a74600f472945ff6ff67d8a9ffccaf322d86d
phonxaychampasouk <phonxay.champasouk@outlook.com>
2020-02-26 16:39:04 -0800

    Defined server connection

--- server/models/index.js
+++ server/models/index.js
@@ -2,7 +2,14 @@ var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {}, // a function which produces all the messages
+    get: function () {
+      // Ask the SQL database for a list of all the messages, including username and roomname
+
+      // Put it into an object
+
+      // Send it back to the client
+
+    }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
 
--- server/schema.sql
+++ server/schema.sql
@@ -2,18 +2,23 @@ CREATE DATABASE chat;
 
 USE chat;
 
-CREATE TABLE messages (
-  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-  username varchar(15),
-  message varchar(80)
-);
-
 /* Create other tables and define schemas for them here! */
 
+CREATE TABLE users (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  username varchar(50)
+);
+
+CREATE TABLE messages (
+  id INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  messageText varchar(50),
+  roomname varchar(50),
+  userID varchar(50)
+);
 
 
 
 /*  Execute this file from the command line by typing:
- *    mysql -u root < server/schema.sql
+ *    mysql -u student < server/schema.sql
  *  to create the database and the tables.*/
 
