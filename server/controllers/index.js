dd2e351998e2c593a0fcd63ee5bfd8276a3ca8b7
phonxaychampasouk <phonxay.champasouk@outlook.com>
2020-02-26 18:30:23 -0800

    Continue writing server responses

--- server/controllers/index.js
+++ server/controllers/index.js
@@ -1,8 +1,24 @@
+// SERVER >>>> CLIENT
+
 var models = require('../models');
 
 module.exports = {
   messages: {
-    get: function (req, res) {}, // a function which handles a get request for all messages
+    get: function (req, res) { // a function which handles a get request for all messages
+    // 1) ask models to make the SQL request
+    // 2) send back the response
+      models.messages.get( (err, results) => {
+        if (err) {
+          res.statusCode = 404;
+          res.end();
+        } else {
+          res.statusCode = 200;
+          res.write(JSON.stringify(results));
+          res.end();
+        }
+      });
+    },
+
     post: function (req, res) {} // a function which handles posting a message to the database
   },
 
--- server/models/index.js
+++ server/models/index.js
@@ -1,14 +1,19 @@
+// SERVER >>>> DATABASE
+
 var db = require('../db');
 
 module.exports = {
   messages: {
-    get: function () {
+    get: function (callback) {
       // Ask the SQL database for a list of all the messages, including username and roomname
-
-      // Put it into an object
-
+      db.connection.query('SELECT * FROM messages', function (error, results, fields) {
+        if (error) {
+          callback(error, null);
+        } else {
+          callback(null, results);
+        }
       // Send it back to the client
-
+      });
     }, // a function which produces all the messages
     post: function () {} // a function which can be used to insert a message into the database
   },
@@ -20,3 +25,4 @@ module.exports = {
   }
 };
 
+module.exports.messages.get();
